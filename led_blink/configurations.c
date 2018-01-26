14b8515f6cd9eed58445645136aaf95e25e50719
Batu <batuhan.kindan@gmail.com>
2018-01-26 10:21:16 +0300

    configuration source file and its header created
    - configurations.c
    - configurations.h

---
+++ led_blink/configurations.c
@@ -0,0 +1,14 @@
+/**
+  ******************************************************************************
+  * @file    configurations.c
+  * @author  Ali Batuhan KINDAN
+  * @version V1.0.0
+  * @date    26 Jan 2018
+  * @brief   This file includes all of the microcontroller hardware configuraiton 
+  *          functions that are used in application	
+  ******************************************************************************
+  */
+	
+	
+#include "configurations.h"
+
