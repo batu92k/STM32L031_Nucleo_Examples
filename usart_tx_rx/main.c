245ccb2c6d730cbe77dcd0f46bcc29c8598de8fa
Batu <batuhan.kindan@gmail.com>
2018-01-27 14:14:02 +0300

    usart_tx_rx project files created

---
+++ usart_tx_rx/main.c
@@ -0,0 +1,27 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ali Batuhan KINDAN
+  * @version V1.0.0
+  * @date    27 Jan 2018
+  * @brief   main program source file
+  ******************************************************************************
+  */
+	
+#include "stm32l0xx.h" // Device header
+
+/**
+  * @brief  Program entry point
+  * @param  none
+  * @retval none
+  */
+int main(void)
+{
+	
+	
+	while(1)
+	{
+		
+	}
+	
+}	
