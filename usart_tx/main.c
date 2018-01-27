d2985611817a6a364f15261ad28be0b4a61db030
Batu <batuhan.kindan@gmail.com>
2018-01-27 14:00:33 +0300

    main.c file updated
    USART mesaage transmiting routines defined in main.c file.

--- usart_tx/main.c
+++ usart_tx/main.c
@@ -9,7 +9,14 @@
   */
 	
 #include "stm32l0xx.h" // Device header
+#include "stdio.h" // standard IO header
+#include "stm32l0xx_ll_usart.h" // USART driver header
+#include "stm32l0xx_ll_gpio.h" // GPIO header
 #include "configurations.h" // Configurations header
+#include "usart_functions.h" // USART functions header
+
+char str[50]; // USART buffer string
+int32_t i = 0; // general purpose
 
 /**
   * @brief  Program entry point
@@ -21,11 +28,25 @@ int main()
 	
 	System_ClockConfiguration(); // configurate system clock
 	
+	USART2_Init(); // inizialize USART2
+	
 	LED3_Init(); // initialize onboard LED
 	
+	/* main loop */
 	while(1)
 	{
-	
+
+		/* USART transmit routine */
+		sprintf(str, "STM32L031 USART Tx Test\n");
+		USART_Puts(USART2, str);
+		
+		i = 150000; // delay value set
+		
+		/* basic delay */
+		while(i)
+		{
+			i--;
+		}
 		
 	}
 				
