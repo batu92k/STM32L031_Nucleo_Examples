9cd7cfa069fc95ef5ac30bc9d8b7d4a8d1e6398c
Batu <batuhan.kindan@gmail.com>
2018-01-27 13:35:31 +0300

    usart_functions source file created
    usart_functions source file and its header created.
    USART_Puts function  and the routines that are used for transmit the
    data thruogh USART defined in souce file.

---
+++ usart_tx/usart_functions.h
@@ -0,0 +1,19 @@
+/**
+  ******************************************************************************
+  * @file    usart_functions.h
+  * @author  Ali Batuhan KINDAN
+  * @version V1.0.0
+  * @date    27 Jan 2018
+  * @brief   This file contains all the function prototypes that are used in the 
+	*          USART process
+  ******************************************************************************
+  */
+
+#ifndef USART_FUNCTIONS_H
+#define USART_FUNCTIONS_H
+
+#include "usart_functions.h" // USART driver header
+
+void USART_Puts(USART_TypeDef* USARTx, volatile char *s);
+
+#endif
