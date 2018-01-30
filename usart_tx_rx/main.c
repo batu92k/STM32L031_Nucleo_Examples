8a81ef601bed1d9a7a79b5ec9c7aac6913faf3e1
Batu <batuhan.kindan@gmail.com>
2018-01-30 11:49:10 +0300

    main.c file updated
    - include sections edited
    - routined in the main loop configured

--- usart_tx_rx/main.c
+++ usart_tx_rx/main.c
@@ -9,7 +9,17 @@
   */
 	
 #include "stm32l0xx.h" // Device header
-#include "configurations.h" // configurations header
+#include "stdio.h" // Standard IO header
+#include "stm32l0xx_ll_usart.h" // USART driver header
+#include "stm32l0xx_ll_gpio.h" // GPIO header
+#include "stm32l0xx_ll_utils.h" // Utilities header
+#include "stm32l0xx_ll_rcc.h" // Reset and Control Clock header
+#include "configurations.h" // Configurations header
+#include "usart_functions.h" // USART functions header
+#include "handlers.h"
+
+char str[50]; // USART buffer string
+static int32_t i = 0; // general purpose counter
 
 /**
   * @brief  Program entry point
@@ -21,11 +31,26 @@ int main(void)
 	
 	System_ClockConfiguration(); // system clock configuration 
 	
-	LED3_Init(); // on board LED initialization
+	LED3_Init(); // onboard LED initialization
+	
+	USART2_Init(); // USART2 initialization
+	
+	
 	
 	/* main loop */
 	while(1)
 	{
+		
+		sprintf(str, "STM32L031 USART Test\n");
+		USART_Puts(USART2, str);
+		
+		i = 1500000;
+		
+		/* basic delay */
+		while(i)
+		{
+			i--;
+		}	
 		
 	}
 	
