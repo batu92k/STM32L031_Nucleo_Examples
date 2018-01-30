5ef7fc40edb17f04f4feb219ebf260ab9d1365e6
Batu <batuhan.kindan@gmail.com>
2018-01-30 11:40:41 +0300

    handlers.c and its header file created
    This file contains all of the interrupt handlers that are used in the
    application

---
+++ usart_tx_rx/handlers.h
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  * @file    handlers.h
+  * @author  Ali Batuhan KINDAN
+  * @version V1.0.0
+  * @date    27 Jan 2018
+  * @brief   This file contains all of the interrupt handler function prototypes 
+	*          that are used in the application
+  ******************************************************************************
+  */
+	
+	
+#ifndef HANDLERS_H
+#define HANDLERS_H
+
+void USART2_IRQHandler(void);
+
+#endif
